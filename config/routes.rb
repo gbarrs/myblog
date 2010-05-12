31747cc6c721873b8f37993a18ff579bd39f438c
Gordon Barrs <gbarrs@Gordon-Barrss-MacBook.local>
2010-05-12 21:34:22 +0100

    checkin after doing basic post and starting Rails Tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :posts
+  map.resources :posts, :has_many => :comments
 
   # The priority is based upon order of creation: first created -> highest priority.
 
@@ -40,6 +40,8 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
+ 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.root :controller => "posts"
 end
